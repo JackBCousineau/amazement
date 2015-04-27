50cb5972a8eff1aab889291c4cda253e03aeab86
Jack Cousineau <jackbcousineau@gmail.com>
2015-04-27 13:11:10 -0700

    Added text displaying level, added increasing wall movement speed with each level

--- src/com/gmail/jackbcousineau/AmazementMain.java
+++ src/com/gmail/jackbcousineau/AmazementMain.java
@@ -1,7 +1,5 @@
 package com.gmail.jackbcousineau;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -9,13 +7,13 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.ParticleEffect;
-import com.badlogic.gdx.graphics.g2d.ParticleEffectPool;
-import com.badlogic.gdx.graphics.g2d.ParticleEffectPool.PooledEffect;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
@@ -32,12 +30,10 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Box2D;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.EdgeShape;
-import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
@@ -45,7 +41,16 @@ import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
 import com.gmail.jackbcousineau.AmazementMain.Maze.MazePiece;
 
+
 public class AmazementMain extends ApplicationAdapter implements InputProcessor{
+
+	@Override
+	public void dispose (){
+		sb.dispose();
+		texture.dispose();
+		world.dispose();
+		collisionSound.dispose();
+	}
 
 	class EdgeBody{
 
@@ -80,7 +85,7 @@ public class AmazementMain extends ApplicationAdapter implements InputProcessor{
 			else if(i == 8) lineShape.set(width, height-100, width+50, height-100);
 			boxFixtureDef.restitution = 0.75f;
 			//boxFixtureDef.restitution = 100f;
-			boxFixtureDef.density = 2.0f;
+			//boxFixtureDef.density = 2.0f;
 			boxFixtureDef.shape = lineShape;
 			lineBody.createFixture(boxFixtureDef);
 			lineShape.dispose();
@@ -93,7 +98,7 @@ public class AmazementMain extends ApplicationAdapter implements InputProcessor{
 
 		class MazeLevel {
 
-			int level;
+			int level, speed = 1;
 			Color color;
 
 			public MazeLevel(int level, Color color){
@@ -101,15 +106,17 @@ public class AmazementMain extends ApplicationAdapter implements InputProcessor{
 				this.color = color;
 				print("Playing level " + level);
 			}
-			
+
 			public void nextLevel(){
 				playerBody.setLinearVelocity(0, 0);
-				playerBody.setTransform(100, 100, playerBody.getAngle());
+				playerBody.setTransform(50, 50, playerBody.getAngle());
 				level++;
+				speed++;
 				print("Playing level " + level);
 				if(color == Color.BLACK) color = Color.WHITE;
 				else color = Color.BLACK;
 				resetAll();
+				displayLevel();
 			}
 		}
 
@@ -126,20 +133,21 @@ public class AmazementMain extends ApplicationAdapter implements InputProcessor{
 		}
 
 		public void turnAllLeft(){
-			//print("TURNING ALL LEFT");
 			for(MazePiece p : pieces){
 				p.rotateLeft();
 			}
 		}
 
 		public void turnAllRight(){
-			//print("TURNING ALL RIGHT");
 			for(MazePiece p : pieces){
 				p.rotateRight();
 			}
 		}
-		
+
 		public void resetAll(){
+			for(MazePiece p : pieces){
+				world.destroyBody(p.piece);
+			}
 			pieces.clear();
 			setPieces();
 		}
@@ -147,12 +155,21 @@ public class AmazementMain extends ApplicationAdapter implements InputProcessor{
 		public Maze(){
 			setPieces();
 		}
-		
+
 		private void setPieces(){
 			addMazePiece(250, 500, 0);
 			addMazePiece(350, 500, 90);
 			addMazePiece(450, 500, 180);
 			addMazePiece(550, 500, 270);
+			addMazePiece(750, 500, 0);
+			addMazePiece(750, 500, 90);
+			addMazePiece(750, 500, 180);
+			addMazePiece(750, 500, 270);
+			//addMazePiece(100, 100, 270);
+			//addMazePiece(0, 100, 0);
+			//addMazePiece(100, 100, 90);
+			//addMazePiece(100, 200, 90);
+			//addMazePiece(100, 400, 270);
 		}
 
 		private void addMazePiece(int x, int y, int degrees){
@@ -163,18 +180,17 @@ public class AmazementMain extends ApplicationAdapter implements InputProcessor{
 
 			public Body piece;
 
-			int x, y, x2, y2, degrees = 0, originalDegrees;
+			int x, y, x2, y2, degrees = 0;
 
-			boolean isMoving = false;
+			boolean isMoving = false, setPos = false;
+			
+			float desiredAngle;
 
-			//orientation: 1 = left-right horizontal, 2 = right-left horizontal, 3 = bottom-up vertical, 4 = top-down vertical
-			//orientation: true = horizontal, false = vertical
 			public MazePiece(int x, int y, int degrees){
 				this.x = x;
 				this.y = y;
 				x2 = x;
 				y2 = y;
-				originalDegrees = degrees;
 				EdgeShape lineShape = new EdgeShape();
 				BodyDef lineBodyDef = new BodyDef();
 				lineBodyDef.type = BodyType.KinematicBody;
@@ -211,11 +227,11 @@ public class AmazementMain extends ApplicationAdapter implements InputProcessor{
 				float desiredAngle = piece.getAngle() + (MathUtils.degreesToRadians * mod);
 				if(rotate){
 					if(mod > 0){
-						piece.setAngularVelocity(1);
+						piece.setAngularVelocity(mazeLevel.speed);
 						scheduleTask(desiredAngle, true);
 					}
 					else if(mod < 0){
-						piece.setAngularVelocity(-1);
+						piece.setAngularVelocity(-mazeLevel.speed);
 						scheduleTask(desiredAngle, false);
 					}
 				}
@@ -237,11 +253,10 @@ public class AmazementMain extends ApplicationAdapter implements InputProcessor{
 
 			class AngleTask extends TimerTask{
 
-				private float desiredAngle;
 				private boolean direction;
 
-				public AngleTask(float desiredAngle, boolean direction){
-					this.desiredAngle = desiredAngle;
+				public AngleTask(float newDesiredAngle, boolean direction){
+					desiredAngle = newDesiredAngle;
 					this.direction = direction;
 				}
 
@@ -253,7 +268,8 @@ public class AmazementMain extends ApplicationAdapter implements InputProcessor{
 					if((angle <= desiredAngle&&direction)||(angle >= desiredAngle&&!direction)) scheduleTask(desiredAngle, direction);
 					else{
 						piece.setAngularVelocity(0);
-						piece.setTransform(x, y, desiredAngle);
+						//piece.setTransform(x, y, desiredAngle);
+						setPos = true;
 						if(degrees == 180||degrees == 0){
 							y2 = (int)y;
 							if(degrees == 0) x2++;
@@ -280,7 +296,7 @@ public class AmazementMain extends ApplicationAdapter implements InputProcessor{
 	Texture texture;
 	Sprite playerSprite;
 
-	boolean leftPressed = false, rightPressed = false, upPressed = false, downPressed = false, gravity = false, drawCollision = false, resetPosition = false;
+	boolean leftPressed = false, rightPressed = false, upPressed = false, downPressed = false, gravity = false, drawCollision = false, resetPosition = false, displayLevel = false;
 
 	World world;
 	Box2DDebugRenderer debugRenderer;
@@ -288,10 +304,32 @@ public class AmazementMain extends ApplicationAdapter implements InputProcessor{
 	EdgeBody edgeBody;
 	Array<Body> bodies = new Array<Body>();
 	Maze maze;
+	Sound collisionSound;
 
 	ParticleEffect collisionEffect;
 
 	Timer timer;
+	
+	BitmapFont font;
+	
+	private void displayLevel(){
+		displayLevel = true;
+		timer.schedule(new TimerTask(){
+
+			@Override
+			public void run() {
+				displayLevel = false;
+			}}, 1500);
+	}
+	
+	/*class DisplayLevelTask extends TimerTask{
+
+
+		@Override
+		public void run() {
+			displayLevel = false;
+		}
+	}*/
 
 	private void createPlayer(){
 		texture = new Texture(Gdx.files.internal("pik.png"));
@@ -310,22 +348,14 @@ public class AmazementMain extends ApplicationAdapter implements InputProcessor{
 		boxFixtureDef.density = 0f;
 		playerBody.createFixture(boxFixtureDef);
 		boxShape.dispose();
-
-
-		//sprite.translate(512, 512);
-		//BodyDef bodyDef = new BodyDef();
-		//bodyDef.type = BodyType.DynamicBody;
-		//bodyDef.bullet = true;
-		//bodyDef.position.set(100, 300);
-		//body = world.createBody(bodyDef);
 		playerBody.setUserData(playerSprite);
 	}
 
 	@Override public void create () {
 		//
 		Box2D.init();
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
+		final float w = Gdx.graphics.getWidth();
+		final float h = Gdx.graphics.getHeight();
 
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false,w,h);
@@ -337,19 +367,16 @@ public class AmazementMain extends ApplicationAdapter implements InputProcessor{
 		debugRenderer = new Box2DDebugRenderer();
 		timer = new Timer();
 
+		collisionSound = Gdx.audio.newSound(Gdx.files.internal("pew.wav"));
+
 		world.setContactListener(new ContactListener() {
 			@Override
 			public void beginContact(Contact contact) {
+				collisionSound.play();
 				//print("CONTACT");
 				//Body a=contact.getFixtureA().getBody();
 				Body b=contact.getFixtureB().getBody();
-				/*if(b.getPosition().x > 1024){
-			    	print("BODY B: x> 1024");
-			    }
-			    if(b.getPosition().y > 1024){
-			    	print("BODY B: y> 1024");
-			    }*/
-				if(b.getPosition().x > 1024||b.getPosition().y > 1024){
+				if(b.getPosition().x > w||b.getPosition().y > h){
 					//sprite.setPosition(500, 500);
 					resetPosition = true;
 				}
@@ -365,7 +392,6 @@ public class AmazementMain extends ApplicationAdapter implements InputProcessor{
 
 			@Override
 			public void endContact(Contact contact) {
-				//print("ENDCONTACT");
 				//crumpit96 or trumpet96
 			}
 
@@ -378,23 +404,22 @@ public class AmazementMain extends ApplicationAdapter implements InputProcessor{
 			}
 		});
 
-		//camera.lookAt(0, 0, 0);
-
-		//camera.translate(0, 534);
-
 		sb = new SpriteBatch();
 
 		createPlayer();
 		edgeBody = new EdgeBody();
 		maze = new Maze();
 
-		ParticleEffectPool effectPool;
-		Array<PooledEffect> effects = new Array<PooledEffect>();
+		//ParticleEffectPool effectPool;
+		//Array<PooledEffect> effects = new Array<PooledEffect>();
 
 		collisionEffect = new ParticleEffect();
 		collisionEffect.load(Gdx.files.internal("collision_particle"), Gdx.files.internal(""));
 		//collisionEffect.setPosition(950, 950);
+        font = new BitmapFont();
+        font.setScale(5);
 		world.getBodies(bodies);
+		displayLevel();
 	}
 
 	public void render () {
@@ -410,6 +435,7 @@ public class AmazementMain extends ApplicationAdapter implements InputProcessor{
 		if(upPressed) playerBody.applyLinearImpulse(0, 5, playerBody.getPosition().x, playerBody.getPosition().y, true);
 		if(downPressed) playerBody.applyLinearImpulse(0, -5, playerBody.getPosition().x, playerBody.getPosition().y, true);
 		sb.begin();
+        if(displayLevel) font.draw(sb, "Level " + maze.mazeLevel.level, 400, 550);
 		//sprite.draw(sb);
 		// I did not take a look at implementation but you get the idea
 		//sprite.setPosition(x, y); = body.localVector.x;
@@ -431,13 +457,15 @@ public class AmazementMain extends ApplicationAdapter implements InputProcessor{
 		//collisionEffect.setPosition(body.getPosition().x, body.getPosition().y);
 		//collisionEffect.draw(sb, Gdx.graphics.getDeltaTime());
 		//}
+		for(MazePiece p : maze.pieces){
+			if(p.setPos){
+				p.setPos = false;
+				p.piece.setTransform(p.x, p.y, p.desiredAngle);
+			}
+		}
 		if(resetPosition){
 			resetPosition = false;
 			maze.mazeLevel.nextLevel();
-			//playerBody.setTransform(100, 100, playerBody.getAngle());
-			//print(body.getLinearVelocity() + "");
-			//playerBody.setLinearVelocity(100, 100);
-			//body.apLinearImpulse(new Vector2(5, 5), body.getPosition(), false);
 		}
 		sb.end();
 		ShapeRenderer shapeRenderer = new ShapeRenderer();
@@ -507,8 +535,6 @@ public class AmazementMain extends ApplicationAdapter implements InputProcessor{
 		Vector3 clickCoordinates = new Vector3(screenX,screenY,0);
 		Vector3 position = camera.unproject(clickCoordinates);
 		playerSprite.setPosition(position.x, position.y);
-		//body.getPosition().set(position.x, position.y);
-		//((Sprite)body.getUserData()).setPosition(position.x, position.y);
 		playerBody.setTransform(position.x, position.y, playerBody.getAngle());
 		return true;
 	}
